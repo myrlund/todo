e63dfce2deeac20ffcd4079d7fd39aed6481824e
Jonas Myrlund <myrlund@gmail.com>
2011-05-14 21:51:16 +0200

    Fixed model permissions in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+  
+  before_filter :set_current_user
+  
+  protected
+  
+  def set_current_user
+    Authorization.current_user = current_user
+  end
+   
+  def render_404(error)
+    puts "Error: #{error}"
+    respond_to do |format|
+      format.html { render :template => "site/welcome" }
+    end
+  end
+    
 end
